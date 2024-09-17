f39dd7d8db662221b749ce3069fb2bcdb5a4304d
Olivier K. <olivierflying321@gmail.com>
2024-09-17 02:17:12 +0200

    Add files via upload

---
+++ props/Multi_prop_sounds/CouldBaAddedToSaberBase_For_multi_prop.h
@@ -0,0 +1,6 @@
+    /* Multi prop effects */                  \
+    DEFINE_EFFECT(BLASTERMODE)                \
+    DEFINE_EFFECT(SABERMODE)                  \
+    DEFINE_EFFECT(DETMODE)                    \
+    DEFINE_EFFECT(JETMODE)                    \
+    DEFINE_EFFECT(DROIDMODE)                  \
